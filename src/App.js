dd14592fd6416d5fc9ec32ca1959fa6bbd38f662
Fatma Hussein <fartunhussein346@gmail.com>
2023-08-22 14:59:35 +0300

    add page routing

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
-import './index.css';
-import Calculator from './components/calculator';
-import FetchQuote from './components/fetchQuote';
+import { Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import CalcPage from './components/CalcPage';
+import QuotePage from './components/QuotePage';
+import Navbar from './components/Nav';
 
 function App() {
   return (
-    <div className="App">
-      <Calculator />
-      <FetchQuote />
-    </div>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="Calculator" element={<CalcPage />} />
+        <Route path="Quotes" element={<QuotePage />} />
+      </Routes>
+    </>
   );
 }
 
